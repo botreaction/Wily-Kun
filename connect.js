1d0b5c0736b480249d46be2ca89a969401a4160a
sinislur <107114276+sinislur@users.noreply.github.com>
2023-09-08 14:48:08 +0700

    Add files via upload

--- connect.js
+++ connect.js
@@ -40,24 +40,12 @@ deviceListMetadata: {},
 }
   return message;
 },
-browser: ['AutoSw','BOT','3.0.0'],
+browser: ['ZassTdr','Chrome','3.0.0'],
 auth: state,
 version
 })
 
 store.bind(ZassTdr.ev)
-
-
- // anticall auto block
-    ZassTdr.ws.on('CB:call', async (json) => {
-    const callerId = json.content[0].attrs['call-creator']
-    if (json.content[0].tag == 'offer') {
-    ZassTdr.sendMessage(callerId, { text: `*Sistem otomatis block!*\n*Jangan menelpon bot*!\n*Silahkan Hubungi Owner Untuk Dibuka !*`}, { quoted : pa7rick })
-        let pa7rick = await ZassTdr.sendContact(callerId, global.owner)
-    await sleep(8000)
-    await ZassTdr.updateBlockStatus(callerId, "block")
-    }
-    })
     
 ZassTdr.ev.process(
 async (events) => {
@@ -139,12 +127,6 @@ return status
 ZassTdr.public = true
 
 ZassTdr.serializeM = (m) => smsg(ZassTdr, m, store)
-
-ZassTdr.ev.on('connection.update', (update) => {
-const {connection,lastDisconnect} = update
-if (connection === 'close') {lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut ? startZassTdr() : ''}
-else if(connection === 'open') {ZassTdr.sendMessage("6289688206739@s.whatsapp.net", {text:`${JSON.stringify(update, undefined, 2)}`})}
-console.log(update)})
 
 ZassTdr.send5ButGif = async (jid , text = '' , footer = '', but = [], options = {}) =>{
 let message = await prepareWAMessageMedia({ video: thumb, gifPlayback: true }, { upload: ZassTdr.waUploadToServer })
